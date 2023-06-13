b9bd7f6605a80c4e985a4908ed28c5f9dceb7208
Wallace Vilela <wallace.vilela9@gmail.com>
2023-06-13 14:58:37 -0300

    Diretiva ngFor

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
+  public eventos: any = [
+    {
+      Tema: 'Angular',
+      Local: 'São Paulo',
+    },
+    {
+      Tema: '.NET5',
+      Local: 'Belo horizonte'
+    },
+    {
+      Tema: 'Angular e suas novidades',
+      Local: 'São paulo'
+    }
+  ];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
