d7ef87b288436674b2f54731fe70b3b6bc6d36d6
Wallace Vilela <wallace.vilela9@gmail.com>
2023-06-15 20:21:22 -0300

    Eventos e editando o nome da controller

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,26 +1,23 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent {
-  public eventos: any = [
-    {
-      Tema: 'Angular',
-      Local: 'São Paulo',
-    },
-    {
-      Tema: '.NET5',
-      Local: 'Belo horizonte'
-    },
-    {
-      Tema: 'Angular e suas novidades',
-      Local: 'São paulo'
-    }
-  ];
-  constructor() {}
+  public eventos: any;
 
-  ngOnInit(): void {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.http.get(`https://localhost:5001/api/eventos`).subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+  }
 }
